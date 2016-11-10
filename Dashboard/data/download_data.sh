394b38427e68e72fa17e19d356bcc1c2113a28ff
chpmoreno <chpmoreno@gmail.com>
2016-11-11 00:22:11 +0100

    fix a bug on download_data.sh

--- Dashboard/data/download_data.sh
+++ Dashboard/data/download_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "This scripts downloads data used in the dashboard"
 
-wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/sentimiento.csv", "sentimiento.csv"
-wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/unifreqdist-bruto.csv", "unifreqdist-bruto.csv"
-wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/bifreqdist-bruto.csv", "bifreqdist-bruto.csv"
-wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/trifreqdist-bruto.csv", "trifreqdist-bruto.csv"
-wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/Series.csv", "Series.csv"
-wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/info_axis.csv", "info_axis.csv"
+wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/sentimiento.csv"
+wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/unifreqdist-bruto.csv"
+wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/bifreqdist-bruto.csv"
+wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/trifreqdist-bruto.csv"
+wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/Series.csv"
+wget "https://dl.dropboxusercontent.com/u/4039122/ISAM/info_axis.csv"
 
